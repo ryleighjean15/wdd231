fa29796f2b2f290535ce93cbe0b39332da071fee
ryleighrowell <ryleighjean15@gmail.com>
2024-09-28 12:26:34 -0700

    directory

---
+++ scripts/prophets.js
@@ -0,0 +1,19 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets); // temporary testing of data response
+  }
+  
+  getProphetData();
+
+  async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+  }
+  
+  getProphetData();
