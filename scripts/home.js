36a30f092deacb62aa390dadfff191464704b6de
ryleighrowell <ryleighjean15@gmail.com>
2024-09-28 16:40:40 -0700

    home page

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the visibility of the menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.querySelector('ul').classList.toggle('show');
+});
