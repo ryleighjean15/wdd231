ef34b9766d646a2b32a4787bb3468d05df33991b
ryleighrowell <ryleighjean15@gmail.com>
2024-10-02 15:16:15 -0700

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,6 +8,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-<script>
-    document.getElementById('lastModified').textContent = document.lastModified;
-</script>
+const gridViewBtn = document.getElementById('gridViewBtn');
+const listViewBtn = document.getElementById('listViewBtn');
+const memberDisplay = document.getElementById('memberDisplay');
+
+gridViewBtn.addEventListener('click', function() {
+  memberDisplay.classList.remove('list-view');
+  memberDisplay.classList.add('grid-view');
+});
+
+listViewBtn.addEventListener('click', function() {
+  memberDisplay.classList.remove('grid-view');
+  memberDisplay.classList.add('list-view');
+});
+
