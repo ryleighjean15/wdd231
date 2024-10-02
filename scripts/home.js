97946fb799b5884a79e63ffbbc91d6a6a9d70933
ryleighrowell <ryleighjean15@gmail.com>
2024-10-02 13:53:25 -0700

    directory

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,8 @@
-// // JavaScript to toggle the visibility of the menu
-// const hamburger = document.getElementById('hamburger');
-// const navMenu = document.getElementById('navMenu');
+// JavaScript to toggle the visibility of the menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
 
-// hamburger.addEventListener('click', () => {
-//     navMenu.querySelector('ul').classList.toggle('show');
-// });
-
-// Toggle the 'show' class on the nav-menu when hamburger is clicked
-document.getElementById('hamburger').addEventListener('click', function() {
-    const navMenu = document.getElementById('navMenu');
-    navMenu.classList.toggle('show');
+hamburger.addEventListener('click', () => {
+    navMenu.querySelector('ul').classList.toggle('show');
 });
+
